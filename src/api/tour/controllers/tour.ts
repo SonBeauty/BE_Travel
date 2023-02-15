8932c399c64093329331e9b22596fcbee3e68eb7
son <sontng@zen-s.com>
2023-02-16 02:19:40 +0700

    create tour

--- src/api/tour/controllers/tour.ts
+++ src/api/tour/controllers/tour.ts
@@ -2,6 +2,22 @@
  * tour controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::tour.tour');
+export default factories.createCoreController("api::tour.tour");
+const { createCoreController } = require("@strapi/strapi").factories;
+const fs = require("fs");
+const { createReadStream, createWriteStream } = require("fs");
+
+module.exports = createCoreController("api::tour.tour", ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.db.query("api::tour.tour").findOne({
+      where: { slug: id },
+    });
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
