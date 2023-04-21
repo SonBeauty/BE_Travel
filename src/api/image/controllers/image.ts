782c20cb24782c9d12e6e2745cb2c1d8a73f1c1e
son <sontng@zen-s.com>
2023-04-21 14:37:32 +0700

    upload image

--- src/api/image/controllers/image.ts
+++ src/api/image/controllers/image.ts
@@ -2,6 +2,45 @@
  * image controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
+const jwt_decode = require("jwt-decode");
 
-export default factories.createCoreController('api::image.image');
+export default factories.createCoreController(
+  "api::image.image",
+  ({ strapi }) => ({
+    async create(ctx) {
+      const { token } = ctx.request.body;
+
+      const result = await strapi.query("api::image.image").create({
+        data: {
+          token,
+        },
+      });
+
+      const decoded = jwt_decode(token);
+
+      const file = ctx.request.files["file"];
+
+      const { path, name, type } = file;
+
+      const files = {
+        path,
+        name,
+        type,
+      };
+
+      console.log(decoded);
+
+      await strapi.plugins.upload.services.upload.upload({
+        data: {
+          refId: decoded.id,
+          ref: "plugin::users-permissions.user",
+          field: "images",
+        },
+        files: files,
+      });
+
+      return result;
+    },
+  })
+);
