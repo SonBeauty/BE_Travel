a88ca59a0e421c97042eb1a2c44a2d2b8688e756
son <sontng@zen-s.com>
2023-04-23 16:45:29 +0700

    delete photos

--- src/extensions/users-permissions/strapi-server.ts
+++ src/extensions/users-permissions/strapi-server.ts
@@ -22,5 +22,42 @@ module.exports = (plugin) => {
     // }
     await register(ctx);
   };
+
+  plugin.controllers.user.deletePhoto = async (ctx) => {
+    const { user } = ctx.state;
+
+    const entry = await strapi.entityService.findOne(
+      "plugin::users-permissions.user",
+      user.id,
+      {
+        populate: "*",
+      }
+    );
+
+    const index = entry.images.findIndex((img) => img.id == ctx.params.id);
+
+    if (index !== -1) {
+      entry.images.splice(index, 1);
+      await strapi.entityService.update(
+        "plugin::users-permissions.user",
+        user.id,
+        {
+          data: {
+            images: entry.images,
+          },
+        }
+      );
+      return { message: "Image deleted successfully" };
+    } else {
+      return ctx.throw(404, "Image not found in user images array");
+    }
+  };
+
+  plugin.routes["content-api"].routes.push({
+    method: "DELETE",
+    path: "/deletePhoto/:id",
+    handler: "user.deletePhoto",
+  });
+
   return plugin;
 };
