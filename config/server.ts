dab6d0ea2a785bcd6b719678b463060cb1d91d48
son <sontng@zen-s.com>
2023-05-02 23:50:46 +0700

    login facebook google

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,8 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
+  url: "https://f53c-2402-800-6279-7e33-5c4c-d2f3-4e4a-bf80.ngrok-free.app",
 });
