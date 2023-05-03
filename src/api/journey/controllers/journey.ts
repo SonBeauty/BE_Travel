347c629e7c7a56fc4af72f379a8d94819829d42a
son <sontng@zen-s.com>
2023-05-03 21:42:49 +0700

    delete

--- src/api/journey/controllers/journey.ts
+++ src/api/journey/controllers/journey.ts
@@ -2,6 +2,14 @@
  * journey controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::journey.journey');
+export default factories.createCoreController(
+  "api::journey.journey",
+  ({ strapi }) => ({
+    async create(ctx) {
+      const response = await super.create(ctx);
+      console.log(response);
+    },
+  })
+);
